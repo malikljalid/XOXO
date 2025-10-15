1d83014302428dd632592950fd26a0699f26591b
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-15 20:29:25 +0100

    create showPlat function to show the plat each time a player make a choice

--- xoxo_mvp.cpp
+++ xoxo_mvp.cpp
@@ -2,16 +2,18 @@
 #include <cstdlib>
 #include <ctime>
 
-enum enGameWinner { PLAYER, COMPUTER, DRAW };
+enum enPlayerSymbol     { X, O };
+enum enGameWinner       { PLAYER, COMPUTER, DRAW };
 
 struct stPlayer
 {
-    short int Choice;
+    enPlayerSymbol  Symbol;
+    short int       Choice;
 };
 
 struct stPlat
 {
-    short int Index[9];
+    char Index[9];
 };
 
 struct stXoxoGame
@@ -22,7 +24,66 @@ struct stXoxoGame
     enGameWinner    Winner;
 };
 
+stPlat initPlat(void)
+{
+    stPlat Plat;
+
+    for (int i = 0; i < 9; i++)
+    {
+        Plat.Index[i] = ' ';
+    }
+
+    return (Plat);
+}
+
+void setPlayerChoice(stPlayer &Player, short int Choice)
+{
+    Player.Choice = Choice;
+}
+
+void setPlayerChoice(stPlat &Plat, stPlayer Player)
+{
+    char xo[2] = {'X', 'O'};
+
+    Plat.Index[Player.Choice] = xo[Player.Symbol];
+}
+
+void showPlat(stPlat Plat)
+{
+    std::cout << std::endl;
+
+    std::cout << "-------------\n";
+    std::cout << "| " << Plat.Index[0] <<" | " << Plat.Index[1] <<" | " << Plat.Index[2] <<" |\n";
+    std::cout << "-------------\n";
+    std::cout << "| " << Plat.Index[3] <<" | " << Plat.Index[4] <<" | " << Plat.Index[5] <<" |\n";
+    std::cout << "-------------\n";
+    std::cout << "| " << Plat.Index[6] <<" | " << Plat.Index[7] <<" | " << Plat.Index[8] <<" |\n";
+    std::cout << "-------------\n";
+
+    std::cout << std::endl;
+}
+
 int main(void)
 {
+    stPlat      Plat = initPlat();
+    stPlayer    Me;
+
+    Me.Symbol = X;
+    Me.Choice = 4;
+
+    showPlat(Plat);
+
+    setPlayerChoice(Plat, Me);
+    showPlat(Plat);
+
+    Me.Choice = 1;
+    setPlayerChoice(Plat, Me);
+    showPlat(Plat);
+
+
+    Me.Choice = 8;
+    setPlayerChoice(Plat, Me);
+    showPlat(Plat);
+
     return (0);
 }
